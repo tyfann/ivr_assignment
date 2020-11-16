f13c5b364469b0256395bc2374e96655d05aae0b
DynamicBB <74177793+DynamicBB@users.noreply.github.com>
2020-11-17 00:04:19 +0800

    Add files via upload

--- src/Untitled-2.py
+++ src/Untitled-2.py
@@ -22,17 +22,66 @@ class image_converter:
     self.bridge = CvBridge()
     self.camera1_data = None
     self.camera2_data = None
+    self.storageR1 = np.array([0.0,0.0,0.0,0.0],dtype='float64')
+    self.storageB1 = np.array([0.0,0.0,0.0,0.0],dtype='float64')
+    self.storageG1 = np.array([0.0,0.0,0.0,0.0],dtype='float64')
+    self.storageT1 = np.array([0.0,0.0,0.0,0.0],dtype='float64')
+
+    self.storageR2 = np.array([0.0,0.0,0.0,0.0],dtype='float64')
+    self.storageB2 = np.array([0.0,0.0,0.0,0.0],dtype='float64')
+    self.storageG2 = np.array([0.0,0.0,0.0,0.0],dtype='float64')
+    self.storageT2 = np.array([0.0,0.0,0.0,0.0],dtype='float64')
+  def poscom(self)
+    if(self.storageB1[0] == 0.0)
+      self.storageB1[0] ==  
 
 
+    else:
+      if(self.camera1_data[0,0] == 0):
+          self.camera1_data[0,0] = self.storageB1[2]*2 - self.storageB1[0]
+          self.camera1_data[0,1] = ((self.storageB1[3]*2 - self.storageB1[1]) + self.camera2_data[0,1])/2
+        
 
-  def callback3(self,data):
-    # Recieve the image
-    try:
-      self.cv_image1 = self.bridge.imgmsg_to_cv2(data, "bgr8")
-    except CvBridgeError as e:
-      print(e)
+      if(self.camera1_data[1,0] == 0):
+          self.camera1_data[1,0] = self.storageG1[2]*2 - self.storageG1[0]
+          self.camera1_data[1,1] = ((self.storageG1[3]*2 - self.storageG1[1]) + self.camera2_data[1,1])/2
+
+      if(self.camera1_data[2,0] == 0):
+          self.camera1_data[2,0] = self.storageR1[2]*2 - self.storageR1[0]
+          self.camera1_data[2,1] = ((self.storageR1[3]*2 - self.storageR1[1]) + self.camera2_data[2,1])/2
+
+      if(self.camera1_data[3,0] == 0):
+          self.camera1_data[3,0] = self.storageT1[2]*2 - self.storageT1[0]
+          self.camera1_data[3,1] = ((self.storageT1[3]*2 - self.storageT1[1]) + self.camera2_data[3,1])/2
     
-    
+      if(self.camera2_data[0,0] == 0):
+          self.camera2_data[0,0] = self.storageB2[2]*2 - self.storageB2[0]
+          self.camera2_data[0,1] = ((self.storageB2[3]*2 - self.storageB2[1]) + self.camera1_data[0,1])/2
+
+      if(self.camera2_data[1,0] == 0):
+          self.camera2_data[1,0] = self.storageG2[2]*2 - self.storageG2[0]
+          self.camera2_data[1,1] = ((self.storageG2[3]*2 - self.storageG2[1]) + self.camera1_data[1,1])/2
+
+      if(self.camera2_data[2,0] == 0):
+          self.camera2_data[2,0] = self.storageR2[2]*2 - self.storageR2[0]
+          self.camera2_data[2,1] = ((self.storageR2[3]*2 - self.storageR2[1]) + self.camera1_data[2,1])/2
+
+      if(self.camera2_data[3,0] == 0):
+          self.camera2_data[3,0] = self.storageT2[2]*2 - self.storageT2[0]
+          self.camera2_data[3,1] = ((self.storageT2[3]*2 - self.storageT2[1]) + self.camera1_data[3,1])/2
+
+
+  def detect_joint_angles(self):
+
+    center = [0,0]
+    circle1Pos = self.camera1_data[0] 
+    circle2Pos = self.camera1_data[1]  
+    circle3Pos = self.camera1_data[2] 
+    # Solve using trigonometry
+    ja1 = np.arctan2(center[0]- circle1Pos[0], center[1] - circle1Pos[1])
+    ja2 = np.arctan2(circle1Pos[0]-circle2Pos[0], circle1Pos[1]-circle2Pos[1]) - ja1
+    ja3 = np.arctan2(circle2Pos[0]-circle3Pos[0], circle2Pos[1]-circle3Pos[1]) - ja2 - ja1
+    return np.array([ja1, ja2, ja3])  
 
 
     # Publish the results
